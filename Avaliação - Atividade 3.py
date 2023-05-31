8bde7e72273e33e462772c33707034c6ee4a0620
matheuscarvalho07 <127998027+matheuscarvalho07@users.noreply.github.com>
2023-05-31 17:53:05 -0300

    Add files via upload

---
+++ Avaliação - Atividade 3.py
@@ -0,0 +1,9 @@
+#Objetivo: Criar um programa que organize uma lista de nomes em ordem alfabetica
+#Bonus: Organizar por ordem alfabetica da ultima letra do nome
+
+names = ["Alexandre", "Alice", "André", "Arthur", "Arthur", "Artur", "Augusto", "Bernardo", "Bernardo", "Bruno", "Davi", "Diego", "Eduardo", "Fabrício", "Felipe", "Fernando", "Francisco", "Francisco", "Gabriel", "Gabriel", "Giovanna", "Giovanni", "Guilherme", "Guilherme", "Hector", "Henrique", "Inácio", "João", "João", "Joaquim", "Júlia", "Lauren", "Leonardo", "Leonardo", "Lucas", "Marina", "Matheus", "Matheus", "Paula", "Pedro", "Pedro",]
+
+
+ordemalfabética = sorted(names)
+
+print(ordemalfabética)
